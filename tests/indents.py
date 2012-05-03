8ef51bf0bf63d7a9b63fa1b73189e83313dc72f1
Jackson Gatenby <jackson.gatenby@gmail.com>
2012-05-03 14:43:31 +1000

    Bug fix in test case

--- tests/indents.py
+++ tests/indents.py
@@ -2,11 +2,12 @@ import unittest
 import simpatico
 
 class TestIndents(unittest.TestCase):
-    EXPECTED = [(6, 'Indentation error (expected 4, got 8)'),
-                (10, 'Indentation error (expected 8, got 7)'),
-                (16, 'Indentation error (expected 12, got 8)'),
-                (20, 'Indentation error (expected 8, got 4)'),
-                (23, 'Indentation error (expected 0, got 4)')]
+    EXPECTED = [(3, 'Indentation error (expected 0, got 4)'),
+                (7, 'Indentation error (expected 4, got 8)'),
+                (11, 'Indentation error (expected 8, got 7)'),
+                (17, 'Indentation error (expected 12, got 8)'),
+                (21, 'Indentation error (expected 8, got 4)'),
+                (24, 'Indentation error (expected 0, got 4)')]
     def runTest(self):
         lines = simpatico.get_lines('tests/files/indents.c')
         errors = simpatico.check_indents(lines)
