557407a769a7ce5e65950ec72c59af076f3a7829
Jackson Gatenby <jackson.gatenby@gmail.com>
2012-12-20 01:48:30 +1000

    Implemented goto check, and started on names checking.

--- rules.py
+++ rules.py
@@ -3,7 +3,7 @@ __all__ = ['get_checkers']
 
 class RuleChecker(object):
     """An abstract class for checking and reporting on rules."""
-    _CATEGORY = None
+    _CATEGORY = NotImplemented
 
     def __init__(self):
         self._errors = []
@@ -13,22 +13,48 @@ class RuleChecker(object):
         self._errors.append((linenum, self._CATEGORY, message))
 
     def check(self, reader):
-        pass
+        raise NotImplementedError
 
     def report(self):
         return sorted(self._errors)
 
-class LineLengthChecker(RuleChecker):
-    """Check line length rules."""
-    _CATEGORY = "Line Length"
+class NamesChecker(RuleChecker):
+    """Checker for naming conventions."""
+    _CATEGORY = "Naming"
+
+    def _check_variable(self, name):
+        # variable names are in camelCase
+        return name[0].islower() and '_' not in name
+
+    def _check_define(self, name):
+        # #defines are in ALL_CAPS
+        return name.isupper() or name in {'true', 'false'}
+
+    def _check_function(self, name):
+        # function names are in all_lowercase
+        return name.islower()
+
+    def _check_type(self, name):
+        # type names are in UpperCamelCase
+        return name[0].isupper() and '_' not in name
 
     def check(self, reader):
-        # Lines > 79 characters are in error.
-        while not reader.end_lines():
-            line = reader.next_line().rstrip('\n')
-            if len(line) > 79:
-                self._error(reader.linenum(),
-                        "Line is {0} characters".format(len(line)))
+        # TODO: know what kind of identifier each identifier is.
+        while not reader.end():
+            token = reader.next_tok()
+            if token.type != 'identifier':
+                continue
+            
+            if not self._check_variable(token.value):
+                self._error(reader.linenum(), "Invalid variable " + token.value)
+
+class BracesChecker(RuleChecker):
+    _CATEGORY = "Braces"
+    # TODO
+
+class IndentationChecker(RuleChecker):
+    _CATEGORY = "Indentation"
+    # TODO
 
 class WhitespaceChecker(RuleChecker):
     """Checker for horizontal and vertical whitespace rules."""
@@ -59,7 +85,30 @@ class WhitespaceChecker(RuleChecker):
             if not correct:
                 self._error(reader.linenum(), message)
 
-_CHECKERS = [LineLengthChecker, WhitespaceChecker]
+class LineLengthChecker(RuleChecker):
+    """Checker for line length rules."""
+    _CATEGORY = "Line Length"
+
+    def check(self, reader):
+        # Lines > 79 characters are in error.
+        while not reader.end_lines():
+            line = reader.next_line().rstrip('\n')
+            if len(line) > 79:
+                self._error(reader.linenum(),
+                        "Line is {0} characters".format(len(line)))
+
+class OverallChecker(RuleChecker):
+    _CATEGORY = "Overall"
+
+    def check(self, reader):
+        # TODO function length (if strict)
+        while not reader.end():
+            token = reader.next_tok()
+            if token.type == 'goto':
+                self._error(reader.linenum(), 'goto')
+
+_CHECKERS = [NamesChecker, #BracesChecker, IndentationChecker,
+        WhitespaceChecker, LineLengthChecker, OverallChecker]
 
 def get_checkers():
     return [c() for c in _CHECKERS]
---
+++ tests/files/goto.c
@@ -0,0 +1,6 @@
+int main(int argc, char** argv) {
+    goto foo;
+    1 / 0;
+    foo:    
+    return 0;
+}
